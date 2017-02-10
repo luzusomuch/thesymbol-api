9fd2d3f3925f486fce50d2d9d55f4cab0eafb162
Vinh Nguyen <luzusomuch@gmail.com>
2017-02-10 10:49:18 +0700

    [-] fix bugs

--- routes/api/v1/currency.js
+++ routes/api/v1/currency.js
@@ -6,11 +6,22 @@ var auth = require(ROOT_FOLDER + "/middlewares/authentication");
 router.get("/", currencyController.query);
 router.get("/get-by-country-code", currencyController.getByCountryCode);
 router.get("/:id", currencyController.fetch);
-router.post("/", passport.authenticate("token", {
+router.post("/", 
+	passport.authenticate("token", {
     session: false
-  }), auth.isAdmin, currencyController.create);
-router.delete("/:id", passport.authenticate("token", {
-  session: false
-}), auth.isAdmin, currencyController.remove);
-router.put("/:id", currencyController.update);
+  }), 
+  auth.isAdmin, 
+  currencyController.create);
+router.delete("/:id", 
+	passport.authenticate("token", {
+  	session: false
+	}), 
+	auth.isAdmin, 
+	currencyController.remove);
+router.put("/:id", 
+	passport.authenticate("token", {
+  	session: false
+  }),
+  auth.isAdmin,
+	currencyController.update);
 module.exports = router;
