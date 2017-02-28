5e7523b4dc7bd754f4173d7b11084d4a74c2e686
Vinh Nguyen <luzusomuch@gmail.com>
2017-02-28 15:28:43 +0700

    [-] change to use romaios fb auth

--- config/social_auth.js
+++ config/social_auth.js
@@ -1,7 +1,15 @@
 module.exports = {
+    // the symbol auth
+    // facebookAuth: {
+    //     clientID: '1019102448207768',
+    //     clientSecret: 'aa8a75739646946aa6654dd593b5bb31',
+    //     scope: 'email, public_profile',
+    //     callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    // },
+    // romaios auth
     facebookAuth: {
-        clientID: '1019102448207768',
-        clientSecret: 'aa8a75739646946aa6654dd593b5bb31',
+        clientID: '733572623474050',
+        clientSecret: '9d1c3d492e398b027f3742a002e2bba5',
         scope: 'email, public_profile',
         callbackURL: 'http://localhost:3000/auth/facebook/callback',
     },
